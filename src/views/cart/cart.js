7cacf29dcb95b134ed8220d5b9099c8fc6db2a59
[SW2/서울/2반] 김소리 <sorisa4@gmail.com>
2022-05-26 17:07:56 +0900

    추가: 장바구니 전체 선택 기능 구현

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -1,11 +1,12 @@
-let storageItems = [];
+let itemStorage = [];
+let orderStorage = [];
 
 function makeCartList(data, sameProductCount) {
   const id = data._id;
   const cartProducts = document.querySelector('#cartProductsContainer');
   const cartItem = `<div class="cart-product-item" id="productItem-${data._id}">
   <label class="checkbox">
-    <input type="checkbox" id="checkbox-${id}" checked="" />
+    <input type="checkbox" id="checkbox-${id}" checked class="check-item" />
   </label>
   <button class="delete-button" id="delete-${id}">
     <span class="icon">
@@ -67,17 +68,15 @@ cartProducts.insertAdjacentHTML('beforeend', cartItem);
 }
 
 document.querySelector('.cart-products-container').addEventListener('click', (e) => {
+  //개별 삭제
   if(e.target.classList.contains('fa-trash-can')) {
     const deleteItemTarget = e.target.parentElement.parentElement.parentElement;
-    // console.log(deleteItemTarget.id);
     const id = deleteItemTarget.id.substring(12);
-    console.log(id);
-    storageItems = storageItems.filter(item => item._id !== id);
-    sessionStorage.setItem('cart', JSON.stringify(storageItems));
+    itemStorage = itemStorage.filter(item => item._id !== id);
+    sessionStorage.setItem('cart', JSON.stringify(itemStorage));
     deleteItemTarget.remove();
-    
   }
-})
+});
 
 // 왜 
 // if(e.target.classList.contains('delete-button')) {
@@ -85,13 +84,29 @@ document.querySelector('.cart-products-container').addEventListener('click', (e)
 // }
 // 하면 작동하지 않지?
 
+//전체 선택
+document.querySelector('#allSelectCheckbox').addEventListener('click', (e) => {
+  const checkboxes = document.querySelectorAll('.check-item');
+  for(let i = 0; i< checkboxes.length; i++) {
+    checkboxes[i].checked = e.target.checked;
+  }
+})
+
+
 const savedCartItems = sessionStorage.getItem('cart');
+const totalPriceExceptDelivery = document.querySelector('#productsTotal');
+const totalPrice = document.querySelector('.total-price')
+
 
 if(savedCartItems !== null) {
   const parsedCartItems = JSON.parse(savedCartItems);
-  storageItems = parsedCartItems;
-  console.log(storageItems);
+  itemStorage = parsedCartItems;
+  let productPriceSum = 0;
   parsedCartItems.forEach(element => {
     makeCartList(element, 1);
+    productPriceSum += element.price;
   });
+  totalPriceExceptDelivery.innerHTML = productPriceSum;
+  totalPrice.innerHTML = parseInt(totalPriceExceptDelivery.innerText) + 3000;
 }
+
