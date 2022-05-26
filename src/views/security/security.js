95bacd071afba3e35df2a2bf1773fb4a41c420df
jinwoo <wwo1278@gmail.com>
2022-05-26 11:22:56 +0900

    모달 구현

--- src/views/security/security.js
+++ src/views/security/security.js
@@ -15,6 +15,11 @@ const address1Input = document.getElementById('address1Input');
 const address2Input = document.getElementById('address2Input');
 const phoneNumberInput = document.getElementById('phoneNumberInput');
 
+const saveButton = document.getElementById('saveButton');
+const modal = document.getElementById('modal');
+const modalCloseButton = document.getElementById('modalCloseButton');
+const saveCompleteButton = document.getElementById('saveCompleteButton');
+
 addAllElements();
 addAllEvents();
 
@@ -29,6 +34,9 @@ function addAllEvents() {
   passwordToggle.addEventListener('click', passwordToggleEvent);
   addressToggle.addEventListener('click', addressToggleEvent);
   phoneNumberToggle.addEventListener('click', phoneNumberToggleEvent);
+  saveButton.addEventListener('click', saveButtonEvent);
+  modalCloseButton.addEventListener('click', modalCloseButtonEvent);
+  saveCompleteButton.addEventListener('click', saveCompleteButtonEvent);
 }
 
 async function getUserData() {
@@ -49,16 +57,60 @@ async function getUserData() {
 
 function fullNameToggleEvent(e) {
   fullNameInput.disabled = !e.target.checked;
+  fullNameInput.focus();
 }
 function passwordToggleEvent(e) {
   passwordInput.disabled = !e.target.checked;
   passwordConfirmInput.disabled = !e.target.checked;
+  passwordInput.focus();
 }
 function addressToggleEvent(e) {
   postalCodeInput.disabled = !e.target.checked;
   address1Input.disabled = !e.target.checked;
   address2Input.disabled = !e.target.checked;
+  address1Input.focus();
 }
 function phoneNumberToggleEvent(e) {
   phoneNumberInput.disabled = !e.target.checked;
+  phoneNumberInput.focus();
+}
+function saveButtonEvent(e) {
+  e.preventDefault();
+  openModal(modal);
+}
+function modalCloseButtonEvent(e) {
+  e.preventDefault();
+  closeModal(modal);
+}
+
+function saveCompleteButtonEvent(e) {
+  e.preventDefault();
+}
+
+// Add a click event on various child elements to close the parent modal
+(
+  document.querySelectorAll(
+    '.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button',
+  ) || []
+).forEach(($close) => {
+  const $target = $close.closest('.modal');
+
+  $close.addEventListener('click', () => {
+    closeModal($target);
+  });
+});
+
+document.onkeydown = function (evt) {
+  evt = evt || window.event;
+  if (evt.keyCode == 27) {
+    closeModal(modal);
+  }
+};
+
+function openModal($el) {
+  $el.classList.add('is-active');
+}
+
+function closeModal($el) {
+  $el.classList.remove('is-active');
 }
