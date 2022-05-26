1dc7e30620c735534fc015bb0a657a7505fc0497
jinwoo <wwo1278@gmail.com>
2022-05-26 14:34:32 +0900

    수정: 제품판매 기능구현
    Updates:
    
    - add.html 수정
    - add.js 데이터 전송 valid 체크
    - add.css 수정
    
    Misc:
    
    todo: api 호출

--- src/views/add/add.js
+++ src/views/add/add.js
@@ -1,11 +1,56 @@
-// 아래는 현재 home.html 페이지에서 쓰이는 코드는 아닙니다.
-// 다만, 앞으로 ~.js 파일을 작성할 때 아래의 코드 구조를 참조할 수 있도록,
-// 코드 예시를 남겨 두었습니다.
-
 import * as Api from '/api.js';
-import { randomId, logout } from '/useful-functions.js';
-import Menu from './component/Menu.js';
-// 요소(element), input 혹은 상수
 
-const logoutBtn = document.querySelector('.logout-btn');
-logoutBtn.addEventListener('click', logout);
+const titleInput = document.getElementById('titleInput');
+const categorySelectBox = document.getElementById('categorySelectBox');
+const manufacturerInput = document.getElementById('manufacturerInput');
+const shortDescriptionInput = document.getElementById('shortDescriptionInput');
+const detailDescriptionInput = document.getElementById('detailDescriptionInput');
+const imageInput = document.getElementById('imageInput');
+const inventoryInput = document.getElementById('inventoryInput');
+const priceInput = document.getElementById('priceInput');
+const searchKeywordInput = document.getElementById('searchKeywordInput');
+const keywordContainer = document.getElementById('keywordContainer');
+const submitButton = document.getElementById('submitButton');
+
+addAllElements();
+addAllEvents();
+
+async function addAllElements() {}
+
+function addAllEvents() {
+  submitButton.addEventListener('click', submitButtonEvent);
+}
+
+function submitButtonEvent(e) {
+  e.preventDefault();
+  if (!titleInput.value) {
+    titleInput.focus();
+    return;
+  }
+
+  if (!categorySelectBox.value) {
+    categorySelectBox.focus();
+    return;
+  }
+
+  if (!manufacturerInput.value) {
+    manufacturerInput.focus();
+    return;
+  }
+
+  if (!shortDescriptionInput.value) {
+    shortDescriptionInput.focus();
+    return;
+  }
+
+  if (!inventoryInput.value) {
+    inventoryInput.focus();
+    return;
+  }
+
+  if (!priceInput.value) {
+    priceInput.focus();
+    return;
+  }
+  alert(priceInput.value);
+}
