9f8e6b2e09fc51d821819e887ed9d59ca1e6ae2e
김다윤 <dayoon1124@naver.com>
2022-05-27 21:50:04 +0900

    수정:세션 스토리지에 name, price, quantity만 넘기도록 수정
    Updates:
    
    - None
    
    Misc:
    
    - None

--- src/views/detail/detail.js
+++ src/views/detail/detail.js
@@ -10,6 +10,8 @@ const url = window.location.href
 const urlSplit = url.split("/")
 const urlId = urlSplit[urlSplit.length-2]
 const sessionArr = [];
+const testArr = [];
+const sessionId = [];
 
 
 //fetch 받아오기
@@ -20,11 +22,25 @@ async function getItems(){
 
 const af = sessionStorage.getItem("cart")
 const fa = JSON.parse(af)
+const sd = sessionStorage.getItem("productName")
+const ds = JSON.parse(sd)
 if(fa !== null){
 	for(let i=0; i<fa.length; i++){
 		sessionArr.push(fa[i])
+		testArr.push(ds[i])
 	}
 }
+
+// for(let i=0; i<sessionArr.length; i++){
+// 	const SSname = fa[i]._id;
+// 	const SSprice = fa[i].price;
+// 	const SSquantity = fa[i].quantity;
+// }
+
+// const productInfo = [{
+// 	"name":SSname,
+// 	"price":SSprice
+// }]
 
 
 //데이터 -> HTML 변환 
@@ -42,21 +58,37 @@ function parseToHTML(item){
 
 	//장바구니 추가
 	const cart = document.querySelector(".add-to-cart")
+
 	
 	async function addToCart(e) {
 		e.preventDefault();
-		// object 형태의 객체는 indexof가 먹히지 않아 stringify로 변환해줌
-		let bbbb = sessionArr.map(JSON.stringify)
+		
+		const productInfo = {
+			"name":`${item.name}`,
+			"price":`${item.price}`,
+			"quantity":1
+		}
+		// console.log(JSON.stringify(productInfo.name))
+			// if(sessionArr[i].name === `${item.name}`){
+			// 	alert("해당 제품이 장바구니에 있습니다.")
+			// }
+			
 
-		if(bbbb.indexOf(JSON.stringify(item)) >= 0 ){
+		// if(bbbb.indexOf(JSON.stringify(productInfo)) >= 0 ){
+		// 	alert("해당 제품이 장바구니에 있습니다.")
+		// }
+		if(testArr.indexOf(productInfo.name) >= 0 ){
 			alert("해당 제품이 장바구니에 있습니다.")
 		}
 		else {
-			sessionArr.push(item)
+			sessionArr.push(productInfo)
+			testArr.push(productInfo.name)
 			alert("장바구니에 추가되었습니다.")
 		}
+		
 
 		sessionStorage.setItem("cart", JSON.stringify(sessionArr))
+		sessionStorage.setItem("productName", JSON.stringify(testArr))
 	}
 	cart.addEventListener("click", addToCart)
 }
