31d462b0112cab194f4b86818da609fdf1d07487
[SW2/서울] 김지호 <sokojiho2@gmail.com>
2022-05-27 17:05:42 +0900

    추가: 주문-스키마-모델-서비스-관리자권한
    Updates:
    
    - 주문 스키마 작성
    - 주문 모델 작성
    - 주문 서비스 작성
    - 주문 관리자 권한(주문삭제와 전체주문 불러오기)
    
    Misc:
    
    - None

---
+++ src/db/models/order-model.js
@@ -0,0 +1,38 @@
+import { model } from 'mongoose';
+import { OrderSchema } from '../schemas/order-schema';
+
+const Order = model('orders', OrderSchema);
+
+export class OrderModel {
+  async findById(userId) {
+    const order = await Order.findOne({ userId: userId });
+    return order;
+  }
+
+  async create(orderInfo) {
+    const createdNewOrder = await Order.create(orderInfo);
+    return createdNewOrder;
+  }
+
+  async findAll() {
+    const orderes = await Order.find({});
+    return orderes;
+  }
+
+  async update({ orderId, update }) {
+    const filter = { _id: orderId };
+    const option = { returnOriginal: false };
+
+    const updatedOrder = await Order.findOneAndUpdate(filter, update, option);
+    return updatedOrder;
+  }
+
+  async delete(orderId) {
+    const order = await Order.deleteOne({ _id: orderId });
+    return order;
+  }
+}
+
+const orderModel = new OrderModel();
+
+export { orderModel };
