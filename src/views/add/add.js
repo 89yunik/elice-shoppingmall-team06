d346dfe753b2aab176b2e506c11496725e1931a0
jinwoo <wwo1278@gmail.com>
2022-05-27 16:16:52 +0900

    수정: 키워드 기능 구현
    Updates:
    
    키워드 기능 구현
    
    Misc:
    
    - None

--- src/views/add/add.js
+++ src/views/add/add.js
@@ -10,8 +10,10 @@ const inventoryInput = document.getElementById('inventoryInput');
 const priceInput = document.getElementById('priceInput');
 const searchKeywordInput = document.getElementById('searchKeywordInput');
 const keywordContainer = document.getElementById('keywordContainer');
+const addKeywordButton = document.getElementById('addKeywordButton');
 const submitButton = document.getElementById('submitButton');
 
+const searchKeywordArr = [];
 addAllElements();
 addAllEvents();
 
@@ -34,6 +36,49 @@ async function getCategoryData() {
 
 function addAllEvents() {
   submitButton.addEventListener('click', submitButtonEvent);
+  addKeywordButton.addEventListener('click', addKeywordButtonEvent);
+  searchKeywordInput.addEventListener('keyup', searchKeywordInputEvent);
+}
+
+function searchKeywordInputEvent(e) {
+  e.preventDefault();
+  if (e.key === 'enter') {
+    addKeywordButton.click();
+  }
+}
+
+function addKeywordButtonEvent(e) {
+  e.preventDefault();
+  searchKeywordArr.push(searchKeywordInput.value);
+  searchKeywordInput.value = '';
+
+  keywordsRender();
+}
+
+function keywordsRender() {
+  keywordContainer.innerHTML = searchKeywordArr.map((item, i) => keywordsTemplate(item, i)).join('');
+
+  keywordContainer.addEventListener('click', isDeleteEvent);
+}
+
+function keywordsTemplate(data, i) {
+  return `
+    <div class="control" id="aoreow">
+      <div class="tags has-addons">
+        <span class="tag is-link is-light">${data}</span>
+        <a class="tag is-link is-light is-delete" data-i =${i}></a>
+      </div>
+    </div>
+  `;
+}
+
+function isDeleteEvent(e) {
+  e.preventDefault();
+  const targetIndex = e.target.closest('.is-delete').dataset.i;
+
+  searchKeywordArr.splice(targetIndex, 1);
+
+  keywordsRender();
 }
 
 async function submitButtonEvent(e) {
@@ -77,7 +122,7 @@ async function submitButtonEvent(e) {
     imageUrl: 'https://test.com/', // 수정예정
     stock: inventoryInput.value,
     price: priceInput.value,
-    keywords: ['test1', 'test2'],
+    keywords: searchKeywordArr,
   };
 
   try {
