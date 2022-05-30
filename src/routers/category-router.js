3620f679741c334f48c20498951d4b810a306864
nickyun <89yunik@gmail.com>
2022-05-30 14:28:29 +0900

    삭제: 실험용 console.log 삭제
    Updates:
    
    - console.log 삭제
    
    Misc:
    
    - None

--- src/routers/category-router.js
+++ src/routers/category-router.js
@@ -16,8 +16,6 @@ categoryRouter.post('/categoryregister', async (req, res, next) => {
 
     // 위 데이터를 카테고리 db에 추가하기
     const newCategory = await categoryService.addCategory(req.body);
-    console.log(nexCategory);
-    console.log('test');
     // 추가된 카테고리의 db 데이터를 프론트에 다시 보내줌
     // 물론 프론트에서 안 쓸 수도 있지만, 편의상 일단 보내 줌
     res.status(201).json(newCategory);
